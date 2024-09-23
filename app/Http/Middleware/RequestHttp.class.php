fafeef2c750d6066db3ba4035d5a2a7fb5da24f6
devirich <devtemplate0@gmail.com>
2024-09-23 22:02:11 +0700

    Update RequestHttp.class.php

--- app/Http/Middleware/RequestHttp.class.php
+++ app/Http/Middleware/RequestHttp.class.php
@@ -2,67 +2,85 @@
 
 namespace App\Http\Middleware\RequestHttp;
 
-require_once __DIR__ . './../../../lib/autoload.php';
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+require_once __DIR__ . './../../../lib/guzzle/autoload.php';
 
 class RequestClient
 {
-    public $options = [
-        //'base_uri' => '',
-        'timeout' => 3.0,
-        /* 'headers' => [
-            'Accept' => 'application/json',
-            'Content-Type' => 'application/json'
-        ] */
+    private $client;
+    private $header = [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json; charset=utf-8'
     ];
 
-    public function setDefault($options)
+    public function __construct($baseUri)
     {
-        $this->options = $options;
+        $this->client = new Client([
+            'base_uri' => $baseUri
+        ]);
     }
 
-    public function get($url, $headers = [], $body = [])
+    public function get($uri)
     {
-        $client = new \GuzzleHttp\Client($this->options);
-        $response = $client->request('GET', $url, [
-            'headers' => $headers,
-            'query' => $body
-        ]);
+        try {
+            $response = $this->client->request('GET', $uri, [
+                'headers' => $this->header,
+                'timeout' => 10
+            ]);
+            return $this->handleResponse($response);
+        } catch (RequestException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function post($uri, $data = [])
+    {
+        try {
+            $response = $this->client->request('POST', $uri, [
+                'headers' => $this->header,
+                'json' => $data
+            ]);
+            return $this->handleResponse($response);
+        } catch (RequestException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function put($uri, $data = [])
+    {
+        try {
+            $response = $this->client->request('PUT', $uri, [
+                'headers' => $this->header,
+                'json' => $data
+            ]);
+            return $this->handleResponse($response);
+        } catch (RequestException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function delete($uri)
+    {
+        try {
+            $response = $this->client->request('DELETE', $uri, [
+                'headers' => $this->header
+            ]);
+            return $this->handleResponse($response);
+        } catch (RequestException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    private function handleResponse($response)
+    {
+        $stCode = $response->getStatusCode();
         $body = $response->getBody();
-        $result = $body->getContents($body);
-        $res = json_decode($result);
-        return $res;
-    }
-
-    public function post($url, $headers = [], $body = [])
-    {
-        $client = new \GuzzleHttp\Client($this->options);
-        $response = $client->request('POST', $url, [
-            'headers' => $headers,
-            'json' => $body,
-            'debug' => true
-        ]);
-        $body = $response->getBody();
-        $result = $body->getContents($body);
-        $res = json_decode($result);
-        return $res;
-    }
-
-    public function put($url, $headers = [], $body = [])
-    {
-        $client = new \GuzzleHttp\Client($this->options);
-        $response = $client->request('PUT', $url, [
-            'headers' => $headers,
-            'body' => $body
-        ]);
-        return $response;
-    }
-
-    public function delete($url, $headers = [])
-    {
-        $client = new \GuzzleHttp\Client($this->options);
-        $response = $client->request('DELETE', $url, [
-            'headers' => $headers,
-        ]);
-        return $response;
+        $bodyArr = json_decode($body, true);
+        return [
+            'status_code' => $stCode,
+            'body' => $bodyArr
+        ];
     }
 }
